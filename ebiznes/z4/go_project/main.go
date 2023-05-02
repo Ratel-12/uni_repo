09da0848d71c361fc3f9e69d12deabfc255700cd
Ratel-12 <lukaszdobrzycki@op.pl>
2023-05-03 00:00:23 +0200

    cleaned up some stuff.

--- ebiznes/z4/go_project/main.go
+++ ebiznes/z4/go_project/main.go
@@ -15,37 +15,28 @@ type Product struct {
 }
 
 func main() {
-	// Connect to the database
 	db, err := gorm.Open(sqlite.Open("products.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	// Migrate the schema
 	db.AutoMigrate(&Product{})
 
-	// Create a new Echo instance
 	e := echo.New()
 
-	// Create a new product
 	e.POST("/products", func(c echo.Context) error {
-		// Parse the request body
 		p := new(Product)
 		if err := c.Bind(p); err != nil {
 			return err
 		}
 
-		// Save the product to the database
 		db.Create(&p)
 
 		return c.JSON(http.StatusCreated, p)
 	})
 
-	// Retrieve a product
 	e.GET("/products/:id", func(c echo.Context) error {
-		// Get the product ID from the URL params
 		id := c.Param("id")
 
-		// Find the product in the database
 		var p Product
 		if err := db.First(&p, id).Error; err != nil {
 			return err
@@ -54,48 +45,37 @@ func main() {
 		return c.JSON(http.StatusOK, p)
 	})
 
-	// Update a product
 	e.PUT("/products/:id", func(c echo.Context) error {
-		// Get the product ID from the URL params
 		id := c.Param("id")
 
-		// Find the product in the database
 		var p Product
 		if err := db.First(&p, id).Error; err != nil {
 			return err
 		}
 
-		// Parse the request body
 		if err := c.Bind(&p); err != nil {
 			return err
 		}
 
-		// Update the product in the database
 		db.Save(&p)
 
 		return c.JSON(http.StatusOK, p)
 	})
 
-	// Delete a product
 	e.DELETE("/products/:id", func(c echo.Context) error {
-		// Get the product ID from the URL params
 		id := c.Param("id")
 
-		// Find the product in the database
 		var p Product
 		if err := db.First(&p, id).Error; err != nil {
 			return err
 		}
 
-		// Delete the product from the database
 		db.Delete(&p)
 
 		return c.NoContent(http.StatusNoContent)
 	})
 
-	// List all products
 	e.GET("/products", func(c echo.Context) error {
-		// Find all products in the database
 		var products []Product
 		if err := db.Find(&products).Error; err != nil {
 			return err
@@ -104,6 +84,5 @@ func main() {
 		return c.JSON(http.StatusOK, products)
 	})
 
-	// Start the server
 	e.Logger.Fatal(e.Start(":8080"))
 }
